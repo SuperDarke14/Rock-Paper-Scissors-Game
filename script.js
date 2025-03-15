7fa157822be2e34c3288fce52d00d99a904f2bf0
Sean Markham <sean@seanmarkham.com>
2025-03-14 22:33:45 -0600

    Refine logic; delete duplicates; use unused vars

--- script.js
+++ script.js
@@ -2,20 +2,24 @@
 console.log(`Welcome to Rock Paper Scissors with a\n
 	statistically perfect opponent`
 );
-function rockPaperScissors (){
+function rockPaperScissors(){
 	//New variable to store user input
 	const user_input = prompt(`Do you choose rock, paper, or scissors?`);
 	//User input into variable
-	
+
+	console.log(user_input);
 
 	//Determine randomly computer's chocie
 	const compChoice = computerChoice();
+	console.log(compChoice);
 	//Compare User's choice with computer's choice and determine winner
+	
 	determineWinner(user_input, compChoice);
 }
-function computerChoice () {
+function computerChoice(){
 	//pick number 1 to 3
-	const choiceIndex = Math.floor(Math.random() * 3); //why is there no easy way to generate an int :/ 
+	const choiceIndex = (Math.floor(Math.random() * 3)-1); //why is there no easy way to generate an int :/ 
+	//goofy minus one because of array indexing from 0
 	//initialize array of [rock, paper, scissors]
 	const rpsArray = ['rock', 'paper', 'scissors'];
 	//return number's index's string
@@ -24,7 +28,7 @@ function computerChoice () {
 
 function determineWinner(user_input, compChoice) {
 	const temp = user_input.toLowerCase(); //error handling
-	if (user_input === 'rock') {
+	if (temp === 'rock') {
 		switch (compChoice) {
 			case 'rock':
 				console.log('tie');
@@ -35,28 +39,8 @@ function determineWinner(user_input, compChoice) {
 			case 'scissors':
 				console.log('You win!');
 				break;
-			default:
-				console.log('Invalid input');
-
 		}
-	}
-	if (user_input === 'rock') {
-		switch (compChoice) {
-			case 'rock':
-				console.log('tie');
-				break;
-			case 'paper':
-				console.log('You lose!');
-				break;
-			case 'scissors':
-				console.log('You win!');
-				break;
-			default:
-				console.log('Invalid input');
-
-		}
-	}
-	if (user_input === 'paper') {
+	} else if (temp === 'paper') {
 		switch (compChoice) {
 			case 'rock':
 				console.log('You win!');
@@ -67,12 +51,8 @@ function determineWinner(user_input, compChoice) {
 			case 'scissors':
 				console.log('You lose');
 				break;
-			default:
-				console.log('Invalid input');
-
 		}
-	}
-	if (user_input === 'scissors') {
+	} else if (temp === 'scissors') {
 		switch (compChoice) {
 			case 'rock':
 				console.log('You lose!');
@@ -83,10 +63,7 @@ function determineWinner(user_input, compChoice) {
 			case 'scissors':
 				console.log('You tie!');
 				break;
-			default:
-				console.log('Invalid input');
-
 		}
-	}
+	} else { console.log('invalid input') };
 }
 
